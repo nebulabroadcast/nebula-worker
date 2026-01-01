cb91fcf79c5cad70aad03b116fe29c25a270f14f
Martastain <martas@imm.cz>
2026-01-01 20:26:44 +0100

    feat: pass samba arguments

--- dispatch/storage_monitor.py
+++ dispatch/storage_monitor.py
@@ -10,13 +10,19 @@ from nebula.storages import Storage
 
 
 def exec_mount(cmd: str) -> bool:
-    proc = subprocess.Popen(cmd, shell=True)
-    while proc.poll() is None:
-        time.sleep(0.1)
-    if proc.returncode:
+    proc = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    if proc.returncode != 0:
+        nebula.log.error(
+            f"Mount failed with return code {proc.returncode}"
+            f": {proc.stderr.decode().strip()}"
+        )
         return False
     return True
-
 
 # def handle_nfs_storage(storage: Storage):
 #     cmd = f"mount.nfs {storage.path} {storage.local_path}"
@@ -38,26 +44,29 @@ def handle_samba_storage(storage: Storage):
             storage.mount_attempts = 999
             return
 
-    nebula.log.info(f"{storage} is not mounted. Mounting...")
+    nebula.log.info(f"Mounting {storage} (attempt {storage.mount_attempts + 1})...")
 
-    smbopts = {}
-    if storage.options.get("login"):
-        smbopts["user"] = storage.options["login"]
-    if storage.options.get("password"):
-        smbopts["pass"] = storage.options["password"]
-    if storage.options.get("domain"):
-        smbopts["domain"] = storage.options["domain"]
+    smbopts = []
+    for key, value in storage.options.items():
+        if key == "login":
+            key = "user"
+        elif key == "password":
+            key = "pass"
+        elif key == "samba_version":
+            key = "vers"
 
-    smbver = storage.options.get("samba_version", "3.0")
-    if smbver:
-        smbopts["vers"] = smbver
+        if value is None:
+            smbopts.append(key)
+        else:
+            smbopts.append(f"{key}={value}")
 
     if smbopts:
-        opts = " -o '{}'".format(",".join([f"{k}={smbopts[k]}" for k in smbopts]))
+        opts = f""" -o '{",".join(smbopts)}'"""
     else:
         opts = ""
 
     cmd = f"mount.cifs {storage.path} {storage.local_path}{opts}"
+    nebula.log.trace(cmd)
 
     if exec_mount(cmd):
         nebula.log.success(f"{storage} mounted successfully")
@@ -76,7 +85,7 @@ class StorageMonitor(BaseAgent):
 
     def main(self):
         db = nebula.DB()
-        db.query("SELECT id, settings FROM storages")
+        db.query("SELECT id, settings FROM storages WHERE enabled")
         status: dict[str, Any] = {}
 
         for id_storage, storage_settings in db.fetchall():
@@ -88,12 +97,9 @@ class StorageMonitor(BaseAgent):
                 )
             )
 
-            storage.last_mount_attempt = self.status.get(id_storage, {}).get(
-                "last_mount_attempt", 0
-            )
-            storage.mount_attempts = self.status.get(id_storage, {}).get(
-                "mount_attempts", 0
-            )
+            stats = self.status.get(id_storage, {})
+            storage.last_mount_attempt = stats.get("last_mount_attempt", 0)
+            storage.mount_attempts = stats.get("mount_attempts", 0)
 
             if storage.is_mounted:
                 continue
